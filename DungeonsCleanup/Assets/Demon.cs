82b2a8bae7b33c59f5a2a7e14e890040e6b2b0a9
azaza12345 <61620351+azaza12345@users.noreply.github.com>
2020-09-07 12:53:13 +0300

    Add Tiles (brake Wall) and Prefab of First Enemy (Demon)

---
+++ DungeonsCleanup/Assets/Demon.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demon : MonoBehaviour
+{
+    Vector3 startPos;
+    Player player;
+    Rigidbody2D myRigidbody;
+    [SerializeField] float speed = 10f;
+    [SerializeField] float distanceToAttack = 15f;
+    Vector3 direction;
+
+    void Awake()
+    {
+        startPos = gameObject.transform.position;
+        player = FindObjectOfType<Player>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        direction =  - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(player.transform.position.x - transform.position.x) < distanceToAttack)
+        {
+            MoveTowardPlayer();
+            return;
+        }
+
+        BackToStartPlace();
+    }
+
+    private void MoveTowardPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    private void BackToStartPlace()
+    {
+        if(transform.position.x != startPos.x)
+            transform.position = Vector3.MoveTowards(transform.position, startPos, speed * Time.deltaTime);
+    }
+}
