297c3fc9dcfa7511e4d4f5c3d6a9dc5bc19b1143
iamvkosarev <iamvkosrev@gmail.com>
2020-09-12 22:52:35 +0300

    Add a Notification Windows (Base and Boss Death). Add Animation for the notification windows
    Co-Authored-By: Vsevolod Kosarev <54035308+iamvkosarev@users.noreply.github.com>

---
+++ DungeonsCleanup/Assets/Scripts/Enemy/BossDeath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDeath : MonoBehaviour
+{
+    [SerializeField] GameObject notificationWindowPrefab;
+    [SerializeField] GameObject playerCanvas;
+    [SerializeField] string name;
+    private void OnDestroy()
+    {
+        GameObject newNotificationWindow = Instantiate(notificationWindowPrefab, playerCanvas.transform);
+        newNotificationWindow.GetComponent<NotificationWindow>().SetButtomPartText($"[{name}]");
+        newNotificationWindow.GetComponent<NotificationWindow>().SetTopPartText($"Внимание");
+    }
+}
