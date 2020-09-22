0e228a99c70d95e15c73bcf993a5be0e146cfb27
iamvkosarev <iamvkosrev@gmail.com>
2020-09-22 15:47:45 +0300

    Сделал систему атаки у гоблинов
    Гоблина в анимации атаки сапаунит объект AttackWave, который в свою очередь ранит заданные объекты и по желанию может сразу уничтожаться.
    
    Co-Authored-By: Vsevolod Kosarev <54035308+iamvkosarev@users.noreply.github.com>

---
+++ DungeonsCleanup/Assets/AttackSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject attackWavePrefab;
+    [SerializeField] Transform pointOfSpwan;
+
+    public void SpawnAttack()
+    {
+        GameObject attackWave = Instantiate(attackWavePrefab, pointOfSpwan.position, Quaternion.identity);
+        if (transform.localScale.x < 0)
+        {
+            attackWave.transform.localScale = 
+                new Vector2(attackWave.transform.localScale.x * -1, attackWave.transform.localScale.y);
+        }
+    }
+}
---
+++ DungeonsCleanup/Assets/DamageDealer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField] Transform damageSpawnPoint;
+    [SerializeField] Vector2 damageZone;
+    [SerializeField] int damage;
+    [SerializeField] LayerMask whomCanAttacked;
+    [SerializeField] bool dontWorkAfterTouch = true;
+    private bool canAttack = true;
+
+    public void SetDamage(int damage)
+    {
+        this.damage = damage;
+    }
+    private void Update()
+    {
+        if (canAttack)
+        {
+            CheckAttackZone();
+        }
+    }
+
+    private void CheckAttackZone()
+    {
+        Collider2D touchedCollider= Physics2D.OverlapBox(damageSpawnPoint.position, damageZone, 0, whomCanAttacked);
+        GameObject touchedObject;
+        if (touchedCollider != null)
+        {
+            touchedObject = touchedCollider.gameObject;
+        }
+        else { return; }
+        Health attackedObjectsHealthScripts = touchedObject.GetComponent<Health>();
+        PlayerHealth attackedObjectsPlayerHealthScripts = touchedObject.GetComponent<PlayerHealth>();
+        if (attackedObjectsHealthScripts != null)
+        {
+            attackedObjectsHealthScripts.TakeAwayHelath(damage);
+            if (dontWorkAfterTouch)
+            {
+                canAttack = false;
+            }
+            return;
+        }
+        if (attackedObjectsPlayerHealthScripts != null)
+        {
+            attackedObjectsPlayerHealthScripts.TakeAwayHelath(damage);
+            if (dontWorkAfterTouch)
+            {
+                canAttack = false;
+            }
+            return;
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(damageSpawnPoint.position, damageZone);
+    }
+    }
