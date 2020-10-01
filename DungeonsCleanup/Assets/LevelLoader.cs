28dabc806298c3759b12f6e5c92fc7e43beef2d7
iamvkosarev <iamvkosrev@gmail.com>
2020-10-01 10:53:46 +0300

    Добавил переходы между сценами
    Co-Authored-By: Vsevolod Kosarev <54035308+iamvkosarev@users.noreply.github.com>

---
+++ DungeonsCleanup/Assets/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] string nextSceneName;
+    [SerializeField] float dalayBeforeStart = 1f;
+    [SerializeField] GameObject canvas;
+    Animator myAnimator;
+    private void Start()
+    {
+        canvas.active = true;
+        myAnimator = GetComponent<Animator>();
+        StartCoroutine(LoadingStartCrossfade());
+    }
+    IEnumerator LoadingStartCrossfade()
+    {
+        yield return new WaitForSeconds(dalayBeforeStart);
+        myAnimator.SetTrigger("StartStartCrossfade");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        myAnimator.SetTrigger("StartExitCrossfade");
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
