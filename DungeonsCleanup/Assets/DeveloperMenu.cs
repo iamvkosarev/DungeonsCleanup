fdbf6e0820fdfe7b9e3ee9a1b0f2bb673d8fab3d
iamvkosarev <iamvkosrev@gmail.com>
2020-10-23 22:10:20 +0300

    Добавил поля и их заполнения в кэнвас прокачки игрока
    Co-Authored-By: Vsevolod Kosarev <54035308+iamvkosarev@users.noreply.github.com>

--- DungeonsCleanup/Assets/DeveloperMenu.cs
+++ DungeonsCleanup/Assets/DeveloperMenu.cs
@@ -1,23 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class DeveloperMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
+    [Header("Manage with Canvases")] 
     [SerializeField] private GameObject developMenuUI;
     [SerializeField] private GameObject gamepadUI;
     [SerializeField] private GameObject playerBarsUI;
+    [Header("Load Player's Data")]
+    [SerializeField] private GameObject player;
+    [SerializeField] private TextMeshProUGUI currentExpTextForm;
+    [SerializeField] private TextMeshProUGUI lvlForm;
+    [SerializeField] private Slider expSlider;
+    [SerializeField] private TextMeshProUGUI needExpForm;
+    [SerializeField] private TextMeshProUGUI hpForm;
+    [SerializeField] private TextMeshProUGUI damageForm;
+
 
     PlayerMovement playerMovement;
+    PlayerDevelopmentManager playerDevManager;
+    PlayerHealth playerHealth;
     private void Start()
     {
-        playerMovement = player.GetComponent<PlayerMovement>();
         CloseDevelopMenu();
     }
     public void OpenDevelopMenu()
     {
         SwitchOtherCanvases(false);
+        LoadDataInForms();
         developMenuUI.SetActive(true);
         if (playerMovement)
         {
@@ -42,4 +56,44 @@ public class DeveloperMenu : MonoBehaviour
         gamepadUI.SetActive(mode);
         playerBarsUI.SetActive(mode);
     }
+
+    #region Load Data
+
+    private void LoadDataInForms()
+    {
+        if (!playerMovement)
+        {
+            playerMovement = player.GetComponent<PlayerMovement>();
+        }
+        if (!playerDevManager)
+        {
+            playerDevManager = player.GetComponent<PlayerDevelopmentManager>();
+        }
+        if (!playerHealth)
+        {
+            playerHealth = player.GetComponent<PlayerHealth>();
+        }
+        LoadDataAboutLvl();
+        LoadDataAboutExp();
+        LoadDataAboutHealthAndDamage();
+    }
+
+
+    private void LoadDataAboutLvl()
+    {
+        lvlForm.text = $"Lvl: {playerDevManager.GetCurrentLvl()}";
+    }
+    private void LoadDataAboutExp()
+    {
+        currentExpTextForm.text = playerDevManager.GetCurrentExp().ToString();
+        needExpForm.text = playerDevManager.GetNeedExp().ToString();
+        expSlider.maxValue = playerDevManager.GetNeedExp();
+        expSlider.value = playerDevManager.GetCurrentExp();
+    }
+    private void LoadDataAboutHealthAndDamage()
+    {
+        hpForm.text = $"{playerHealth.GetHealth()} / {playerDevManager.GetMaxHealthAccordingLvl()}";
+        damageForm.text = playerDevManager.GetDamageAccordingLvl().ToString();
+    }
+    #endregion
 }
