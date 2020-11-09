727a1cefc0ca0ed9acc7389248a4c3f9efe88dad
iamvkosarev <iamvkosrev@gmail.com>
2020-11-09 20:14:38 +0300

    Доработал trap shooter
    @
    
    Co-Authored-By: Vsevolod Kosarev <54035308+iamvkosarev@users.noreply.github.com>

--- DungeonsCleanup/Assets/TrapArrowMovement.cs
+++ DungeonsCleanup/Assets/TrapArrowMovement.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TrapArrowMovement : MonoBehaviour
 {
-    [SerializeField] float speed = 3f;
-    PlayerMovement player;
-    Vector2 velocityDirection;
-    Rigidbody2D myRigidbody;
-    float direction;
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float gravityParam = 3f;
+    [SerializeField] private float delayBeforeGravity = 1f;
+    private PlayerMovement player;
+    private Vector2 velocityDirection;
+    private Rigidbody2D myRigidbody;
+    private Vector2 direction;
+    private bool setVelocity = true;
     public void SetVelocityDirection(Vector2 velocityDirection)
     {
         this.velocityDirection = velocityDirection; 
@@ -17,25 +21,35 @@ public class TrapArrowMovement : MonoBehaviour
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         player = FindObjectOfType<PlayerMovement>();
-        direction = SetDirectionOfShooting();
+        StartCoroutine(GoingDown());
     }
-
+    IEnumerator GoingDown()
+    {
+        yield return new WaitForSeconds(delayBeforeGravity);
+        myRigidbody.gravityScale = gravityParam;
+        setVelocity = false;
+    }
     // Update is called once per frame
     void Update()
     {
-        myRigidbody.velocity = new Vector2(direction * speed, 0);
+        if (setVelocity)
+        {
+            myRigidbody.velocity = new Vector2(direction.x * speed, direction.y * speed);
+        }
+        CheckRotation();
     }
 
-    private float SetDirectionOfShooting()
+    private void CheckRotation()
     {
-        if(transform.position.x <= player.transform.position.x)
-        {
-            return 1;
-        }
-
-        else
-        {
-            return -1;
-        }
+        float angle = (float)Math.Atan2(-myRigidbody.velocity.y, -myRigidbody.velocity.x) * (float)(180 / Math.PI);
+        Debug.Log(angle);
+        gameObject.transform.localRotation = Quaternion.Euler(0f, 0f, angle);
     }
+
+    public void SetDirectionByAngle(float angle)
+    {
+        direction = new Vector2(Mathf.Cos(angle * Mathf.PI / 180f), Mathf.Sin(angle * Mathf.PI / 180f));
+    }
+
+
 }
--- DungeonsCleanup/Assets/TrapShooter.cs
+++ DungeonsCleanup/Assets/TrapShooter.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class TrapShooter : MonoBehaviour
 {
-    [SerializeField] GameObject projectile;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private Transform spawnProjectilesPoint;
+    [Range(0f,360f)] [SerializeField] private float angle = 0f;
     private void Shoot()
     {
-        Instantiate(projectile, transform.position, Quaternion.identity);
+        GameObject projectile =  Instantiate(projectilePrefab, spawnProjectilesPoint.transform.position, Quaternion.identity) as GameObject;
+        TrapArrowMovement trapArrowMovement = projectile.GetComponent<TrapArrowMovement>();
+        trapArrowMovement.SetDirectionByAngle(angle);
+
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(spawnProjectilesPoint.position, new Vector2(Mathf.Cos(angle * Mathf.PI / 180f), Mathf.Sin(angle * Mathf.PI / 180f)));
     }
 }
