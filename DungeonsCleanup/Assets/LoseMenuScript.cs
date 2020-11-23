baafc310209efcce0cac2490adee2af86414f4e1
azaza12345 <61620351+azaza12345@users.noreply.github.com>
2020-11-23 19:24:26 +0300

    Никита, прости, но я всё же добавил рекламу...

---
+++ DungeonsCleanup/Assets/LoseMenuScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class LoseMenuScript : PauseMenu
+{
+    PlayerHealth playerHealth;
+    private void Start()
+    {
+        playerHealth = player.GetComponent<PlayerHealth>();
+
+        //ads
+        if(Advertisement.isSupported)
+        {
+            Advertisement.Initialize("3912667", false);
+        }
+    }
+
+    public void SetLoseCanvas()
+    {
+        gamepadUI.SetActive(false);
+        playerBarsUI.SetActive(false);
+        pauseMenuUI.SetActive(false);
+
+        loseUI.SetActive(true);
+    }
+
+    public void PlayAdvirtisement()
+    {
+        if(Advertisement.IsReady())
+        {
+            Advertisement.Show("video");
+            //Time.timeScale = 0;
+        }
+
+        Debug.Log("YOU HAVE FULL HP NOW!");
+        playerHealth.gameObject.GetComponent<PlayerDataManager>().SetCheckPointSessionData();
+    }
+
+    public void UpdateLastCheckpoint()
+    {
+        playerHealth.gameObject.GetComponent<PlayerDataManager>().SetCheckPointSessionData();
+    }
+
+
+
+
+    
+}
