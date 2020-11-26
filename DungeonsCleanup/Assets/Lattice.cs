082bc719e2adcd41aed261629190b9fa2b8b7c57
iamvkosarev <iamvkosrev@gmail.com>
2020-11-26 22:52:56 +0300

    Добавил префаб решётки, которая перекрывает путь

---
+++ DungeonsCleanup/Assets/Lattice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lattice : MonoBehaviour
+{
+    [SerializeField] private Transform activationPoint;
+    [SerializeField] private Vector2 activationZone;
+    [SerializeField] private LayerMask playerLayer;
+    private Animator myAnimator;
+    private void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        CheckActivationZone();
+    }
+
+    private void CheckActivationZone()
+    {
+        Collider2D collider2D = Physics2D.OverlapBox(activationPoint.position, activationZone, 0, playerLayer);
+        if (collider2D)
+        {
+            myAnimator.Play("Closing Animation");
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawCube(activationPoint.position, activationZone);
+    }
+}
