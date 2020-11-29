31dd31f0adf128d5af978640fc349b62e6dd7dd8
iamvkosarev <iamvkosrev@gmail.com>
2020-11-29 11:41:56 +0300

    Доработал сцены, изменил механику решёток

--- DungeonsCleanup/Assets/Lattice.cs
+++ DungeonsCleanup/Assets/Lattice.cs
@@ -8,6 +8,10 @@ public class Lattice : MonoBehaviour
     [SerializeField] private Transform activationPoint;
     [SerializeField] private Vector2 activationZone;
     [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private bool openLatticeOnBossDeath = false;
+    [SerializeField] private HealthUI bossHealth;
+    private bool wasOpened = true;
+    private bool checkActivationZone = true;
     private Animator myAnimator;
     private void Start()
     {
@@ -16,14 +20,28 @@ public class Lattice : MonoBehaviour
     private void Update()
     {
         CheckActivationZone();
+        ChechBossDeath();
+    }
+
+    private void ChechBossDeath()
+    {
+        if(!openLatticeOnBossDeath) { return; }
+        if(bossHealth.health == 0 && !wasOpened)
+        {
+            wasOpened = true;
+            myAnimator.Play("Opening Animation");
+            checkActivationZone = false;
+        }
     }
 
     private void CheckActivationZone()
     {
+        if (!checkActivationZone) { return; } 
         Collider2D collider2D = Physics2D.OverlapBox(activationPoint.position, activationZone, 0, playerLayer);
         if (collider2D)
         {
             myAnimator.Play("Closing Animation");
+            wasOpened = false;
         }
     }
     private void OnDrawGizmosSelected()
