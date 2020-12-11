4726b0774416904f350e9d35d4db398f4bb4ef92
iamvkosarev <iamvkosrev@gmail.com>
2020-12-11 23:32:36 +0300

    Добавил отображение, к какой точке идёт моб

---
+++ DungeonsCleanup/Assets/Scripts/Gameplay/Movement by Waypoints/WaypointNavigatorEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad()]
+public class WaypointNavigatorEditor
+{
+    [DrawGizmo(GizmoType.Selected)]
+    public static void OnDrawSceneGizmo(WaypointNavigator waypointNavigator, GizmoType gizmoType)
+    {
+        if ((gizmoType & GizmoType.Selected) != 0)
+        {
+            Gizmos.color = Color.red;
+        }
+        Gizmos.DrawSphere(waypointNavigator.currentWaypoint.transform.position, .3f);
+    }
+}
